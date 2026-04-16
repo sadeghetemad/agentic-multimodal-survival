ac3512f502a78da35cfcdf518bebacc8d2c39e60
Sadegh <sadegh.etemad@gmail.com>
2026-04-16 16:52:39 +0100

    feat: add ENV config

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AWS_REGION = os.getenv("AWS_REGION")
+ENV = os.getenv("ENV")
 SAGEMAKER_ENDPOINT = os.getenv("SAGEMAKER_ENDPOINT")
 BEDROCK_MODEL = os.getenv("BEDROCK_MODEL")
 GENOMIC_FG = os.getenv("GENOMIC_FG")
